f2b13864aaaa891ff772d4abe55b2c33253e1b93
Rafael Cabrera <rafaelcabrera@Rafaels-MacBook-Pro.local>
2021-10-10 22:46:50 -0300

    Update Wifi OTA + WifiManager

--- WifiOTA.h
+++ WifiOTA.h
@@ -1,40 +1,25 @@
 #ifndef WifiOTA_h
 #define WifiOTA_h
 
-#include <ESPmDNS.h>
+#include <Wifi.h>  
+#include <WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
+
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+#include "ArduinoOTA.h"   // https://github.com/esp8266/Arduino/tree/master/libraries/ArduinoOTA
 
-#include <Button.h>
-
-
-#if defined(ESP8266)
-#include <ESP8266WiFi.h>  //ESP8266 Core WiFi Library         
-#else
-#include <WiFi.h>      //ESP32 Core WiFi Library    
-#endif
-
-#if defined(ESP8266)
-#include <ESP8266WebServer.h> //Local WebServer used to serve the configuration portal
-#else
-#include <WebServer.h> //Local WebServer used to serve the configuration portal ( https://github.com/zhouhan0126/WebServer-esp32 )
-#endif
-
-#include <DNSServer.h> //Local DNS Server used for redirecting all requests to the configuration portal ( https://github.com/zhouhan0126/DNSServer---esp32 )
-#include <WiFiManager.h>   // WiFi Configuration Magic ( https://github.com/zhouhan0126/WIFIMANAGER-ESP32 ) >> https://github.com/tzapu/WiFiManager (ORIGINAL)
-
+#include <Button.h>       // https://github.com/meirarc/Button.git
 
 class WifiOTA
 {
   public:
-    WifiOTA();
+    WifiOTA(Button button);
     void begin();
-    void handle(Button button);
+    void handle();
 
   private:
-    WiFiManager _wifiManager;
-    void configModeCallback (WiFiManager *myWiFiManager);
-    void saveConfigCallback ();
+    Button _button;
 };
 
 #endif
