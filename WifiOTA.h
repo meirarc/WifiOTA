830c498dabeb10e8f8184d85288c14593fc301a1
Rafael Cabrera <rafaelcabrera@Rafaels-MacBook-Pro.local>
2021-10-11 14:28:02 -0300

    Update OTA lib

--- WifiOTA.h
+++ WifiOTA.h
@@ -1,25 +1,71 @@
 #ifndef WifiOTA_h
 #define WifiOTA_h
 
-#include <WiFi.h>  
+#include <WiFi.h>          //https://github.com/esp8266/Arduino
 #include <WebServer.h>
 #include <DNSServer.h>
-#include <WiFiManager.h>  //https://github.com/zhouhan0126/WIFIMANAGER-ESP32.git
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
 #include <WiFiUdp.h>
-#include "ArduinoOTA.h"   //https://github.com/esp8266/Arduino/tree/master/libraries/ArduinoOTA
+#include <ArduinoOTA.h>
 
-#include <Button.h>       //https://github.com/meirarc/Button.git
+class WifiOTA{
+    public:
+        void begin(){
+            WiFiManager wifiManager;
+            if(!wifiManager.autoConnect(_APName)) {
+                Serial.println("failed to connect and hit timeout");
+                ESP.restart();
+                delay(1000);
+            } 
 
-class WifiOTA
-{
-  public:
-    WifiOTA(Button button);
-    void begin();
-    void handle();
+            ArduinoOTA
+                .onStart([]() {
+                String type;
+                if (ArduinoOTA.getCommand() == U_FLASH)
+                    type = "sketch";
+                else // U_SPIFFS
+                    type = "filesystem";
 
-  private:
-    Button _button;
+                // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+                Serial.println("Start updating " + type);
+                })
+                .onEnd([]() {
+                Serial.println("\nEnd");
+                })
+                .onProgress([](unsigned int progress, unsigned int total) {
+                Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+                })
+                .onError([](ota_error_t error) {
+                Serial.printf("Error[%u]: ", error);
+                if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+                else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+                else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+                else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+                else if (error == OTA_END_ERROR) Serial.println("End Failed");
+                });
+
+            ArduinoOTA.begin();
+
+            Serial.println("connected...yeey :)");
+        };
+
+        void reset(){
+            WiFiManager wifiManager;
+            if (!wifiManager.startConfigPortal(_APName)) {
+                Serial.println("failed to connect and hit timeout");
+                delay(3000);
+                ESP.restart();
+                delay(5000);
+            }
+            Serial.println("connected...yeey :)");
+        };
+
+        void handle(){
+            ArduinoOTA.handle();
+        };
+    private:
+        char* _APName = "OnDemandAP";
 };
 
 #endif
